d6d26d1de062ef314418d7a63e792c52322a5970
benloisch <ben.loisch@gmail.com>
2016-11-19 23:46:40 -0500

    push initial solution

--- Problem 5 Smallest multiple/Source.cpp
+++ Problem 5 Smallest multiple/Source.cpp
@@ -13,14 +13,31 @@ Description:
 This program finds...
 */
 
+int smallestMultiple(int n) {
+	int smallestMultiple = 2;
+	while (true) {
+		int i = 1;
+		for (; i <= n; i++) {
+			if (smallestMultiple % i != 0) {
+				smallestMultiple++;
+				break;
+			}
+		}
 
+		if (i > n)
+			return smallestMultiple;
+	}
+}
 
 int main() {
 
 	//create clock object to track program execution time
 	clock_t begin = clock();
 
-	
+	int n = 20;
+
+	cout << "Smallest number divisible by all numbers from 1 to " << n << " is: " << smallestMultiple(n) << endl;
+
 	clock_t end = clock();
 	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 
