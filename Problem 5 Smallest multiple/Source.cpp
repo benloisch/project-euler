0c5cdc79f1a98be1b41c74006d0a8a46dd8bf94b
benloisch <ben.loisch@gmail.com>
2016-11-21 22:15:30 -0500

    finished problem but needs polish and comments

--- Problem 5 Smallest multiple/Source.cpp
+++ Problem 5 Smallest multiple/Source.cpp
@@ -13,6 +13,7 @@ Description:
 This program finds the smallest number that is evenly divisble by integers 1 through n
 */
 
+//brute force method
 int smallestMultiple(int n) {
 	int number = 2;
 	while (true) {
@@ -36,9 +37,39 @@ int main() {
 	//create clock object to track program execution time
 	clock_t begin = clock();
 
-	int n = 20;
+	//brute force method
+	//int n = 20;
+	//cout << "Smallest number divisible by all integers from 1 to " << n << " is: " << smallestMultiple(n) << endl;
 
-	cout << "Smallest number divisible by all integers from 1 to " << n << " is: " << smallestMultiple(n) << endl;
+	//fast O(n) method
+	//N = number divisible by numbers 1 through k
+	//N = 2^a * 3^b * 5^c * ..... (prime number under k)^z
+	//where 2^a < k && 3^b < k && ... (prime number under k)^z < K
+	//a = floor(log(k) / log(2)), b = floor(log(k) / log(3)), c = floor(log(k) / log5)), etc.
+
+	int k = 20;
+	vector<int> primes; //holds primes (2, 3, 5, 7, 11, ... n while n < k)
+	primes.push_back(2);
+	primes.push_back(3);
+	primes.push_back(5);
+	primes.push_back(7);
+	primes.push_back(11);
+	primes.push_back(13);
+	primes.push_back(17);
+	primes.push_back(19);
+	int N = 1;
+	int currPrime = 2;
+	int i = 0;
+	while (true) {
+		if (i < primes.size()) {
+			int a = floor(log(k) / log(primes[i]));
+			N *= pow(primes[i], a);
+			i++;
+		}
+		else {
+			break;
+		}
+	}
 
 	clock_t end = clock();
 	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
